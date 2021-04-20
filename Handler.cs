fabefd64abfc0cccac996a901295969d1a844277
Almir Zulic <almir@bind.ba>
2021-04-20 14:05:00 +0200

    setup

---
+++ Handler.cs
@@ -0,0 +1,54 @@
+using System;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text.Json;
+
+namespace AwsDotnetCsharp
+{
+    public class Handler
+    {
+
+      private readonly JsonSerializerOptions _options;
+
+      public string FunctionTriggeredBySQS(SQSEvent sqsEvent)
+      {
+
+        Console.WriteLine($"Beginning to process {sqsEvent.Records.Count} records...");
+
+        foreach (var record in sqsEvent.Records)
+        {
+          Console.WriteLine($"Message ID: {record.MessageId}");
+          Console.WriteLine($"Record Body:");
+          Console.WriteLine(record.Body);
+        }
+
+        Console.WriteLine("Processing complete.");
+
+        return $"Processed {sqsEvent.Records.Count} records.";
+      }
+
+      [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.CamelCaseLambdaJsonSerializer))]
+      public APIGatewayProxyResponse SecondFunction(APIGatewayProxyRequest request)
+      {
+        // deserialze json input
+        Request input = JsonSerializer.Deserialize<Request>(request.Body, _options);
+
+        // just return the same to the output 
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(input, _options)
+        };
+  
+      }
+
+    }
+
+    public class Request
+    {
+      public string Key1 {get; set;}
+      public string Key2 {get; set;}
+      public string Key3 {get; set;}
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+**INSTRUCTIONS**
+
+Install dotnetcore 3.1
+
+Install [serverless framework](https://www.serverless.com/framework/docs/providers/aws/guide/installation/) `npm i -g serverless`
+
+*Build*
+
+run `build.cmd` or `./build.sh` depending on your platform
+
+*Deploy*
+
+run `sls deploy`
+
+
